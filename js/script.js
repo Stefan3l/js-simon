af7a52b0158567397711a0d65d300bbc6e6d6ece
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-11 18:53:09 +0100

    fix: js

--- js/script.js
+++ js/script.js
@@ -72,7 +72,27 @@ let message;
         if(numbCpuArray.includes(userNmb[i])) {
             result.push(userNmb[i])
             message =`Hai indovinato  ${result.length} numeri! (${result})`
-            resultElm.innerHTML = message           
+            resultElm.innerHTML = message     
+                    var duration = 15 * 1000;
+        var animationEnd = Date.now() + duration;
+        var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+        function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+        }
+
+        var interval = setInterval(function() {
+        var timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        var particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+        }, 250);      
         }  
            
     }    
@@ -86,23 +106,3 @@ let message;
 
 
 
-// var duration = 15 * 1000;
-// var animationEnd = Date.now() + duration;
-// var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-// function randomInRange(min, max) {
-// return Math.random() * (max - min) + min;
-// }
-
-// var interval = setInterval(function() {
-// var timeLeft = animationEnd - Date.now();
-
-// if (timeLeft <= 0) {
-//     return clearInterval(interval);
-// }
-
-// var particleCount = 50 * (timeLeft / duration);
-// // since particles fall down, start a bit higher than random
-// confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
-// confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
-// }, 250);

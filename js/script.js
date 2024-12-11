d85233159dfd3985658664130f0ffdf22a0815a8
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-11 14:50:23 +0100

    feat: html + css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Elements DOM
+
+const timerElm = document.getElementById(`timer`)
+const numbRandomElm = document.getElementById(`numbers`)
+const userNumbElm = document.getElementById(`numb-utente`)
+const btnElm = document.getElementById(`btn`)
+const resultElm = document.getElementById(`risultato`)
